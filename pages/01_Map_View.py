300f08ef097eab05350f9eae94ab4d1f43e37f8d
Min Ji Choi <min@oxd.com>
2023-04-12 16:36:39 -0700

    Initial design tweaks to adopt look and feel of BC style guidelines.

--- pages/01_Map_View.py
+++ pages/01_Map_View.py
@@ -1,14 +1,33 @@
 import streamlit as st
 import plotly.express as px
 import pydeck as pdk
-
 from utils import load_data
+from PIL import Image
+
+favicon = Image.open("frontend/images/favicon.png")
+st.set_page_config(
+    page_title="DriveBC Camera Dashboard - Map View",
+    page_icon=favicon
+)
+
+with open("frontend/css/style.css") as stylesheet:
+    st.markdown(f'<style>{stylesheet.read()}</style>', unsafe_allow_html=True)
 
 if 'data' not in st.session_state:
     st.session_state['data'] = load_data()
 
 st.title("Camera API Map View")
 
+st.markdown("""
+<nav class="navbar">
+    <div class="banner">
+        <a href="https://gov.bc.ca">
+            <img src="https://developer.gov.bc.ca/static/BCID_H_rgb_rev-20eebe74aef7d92e02732a18b6aa6bbb.svg" alt="Government of British Columbia" class="navbar_logo">
+        </a>
+    </div>
+    <h2 class="navbar_title">DriveBC Camera Dashboard</h2>
+</nav>
+""", unsafe_allow_html=True)
 
 px.set_mapbox_access_token(open(".mapbox_token").read())
 
